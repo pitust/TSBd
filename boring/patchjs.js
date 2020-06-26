32433b1e57bf62b5e8c02b657b136a515da347b0
pitust <stelmaszek.piotrpilot@gmail.com>
2020-06-26 21:38:17 +0100

    stuffs

--- boring/patchjs.js
+++ boring/patchjs.js
@@ -1,4 +1,5 @@
 let fs = require('fs');
 for (let m of process.argv.slice(3)) {
+    if (fs.readFileSync(m).toString().startsWith(`System.register(${JSON.stringify('./' + m.slice(10, -3))},`)) continue;
     fs.writeFileSync(m, `System.register(${JSON.stringify('./' + m.slice(10, -3))}, ` + fs.readFileSync(m).slice(16))
 }
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-rm -rf build
 mkdir -p build/tmp
-tsc --allowJs src/plugin.tsx --checkJs false --outDir build/tmp --module system --target ES2020 --jsx react
+tsc --allowJs src/plugin.tsx --checkJs false --outDir build/tmp --module system --target ES2020 --jsx react --incremental --tsBuildInfoFile build/info.json
 find build/tmp | xargs node boring/patchjs.js
-cat boring/header.js boring/denold.js `find build/tmp` boring/final.js 2>/dev/null >build/AppName.plugin.js
-cp build/AppName.plugin.js ~/.config/BetterDiscord/plugins/ 2>/dev/null
+cat boring/header.js boring/denold.js `find build/tmp` boring/final.js 2>/dev/null >build/badgify.plugin.js
+cp build/badgify.plugin.js ~/.config/BetterDiscord/plugins/ 2>/dev/null
